a0fe7e2313c85882ac9be65eaecc650836835e39
Dani Baeyens <daniel.baeyens@gmail.com>
2013-10-20 13:51:51 +0200

    Details window fields added

--- detailsWindow.js
+++ detailsWindow.js
@@ -9,43 +9,73 @@ const DetailsWindow = new Lang.Class({
     Extends: Gtk.Window,
 
     _init: function(app) {
-	this.parent({ modal: true,
-			     title: "Append a new book",
-			     window_position: Gtk.WindowPosition.CENTER_ON_PARENT,
-			     border_width: 12 });
-	this.app = app;
+        this.parent({ modal: true,
+                      title: "Append a new book",
+                      window_position: Gtk.WindowPosition.CENTER_ON_PARENT,
+                      border_width: 12 });
+        this.app = app;
+
+        this._coverPath;
 
         let dialogGrid = new Gtk.Grid( { row_spacing: 6,
                                          column_spacing: 6 } );
 
+        this._newCoverImage = new Gtk.Button();
         this._newTitleEntry = new Gtk.Entry();
         this._newAuthorEntry = new Gtk.Entry();
-	this._newISBNEntry = new Gtk.Entry();
+        this._newISBNEntry = new Gtk.Entry();
+        this._newYearEntry = new Gtk.Entry();
+        this._newSummaryTextArea = new Gtk.TextView();
+        this._newNotesTextArea = new Gtk.TextView();
+        this._newRating = new Gtk.SpinButton();
 	
 
+        dialogGrid.attach(this._newCoverImage,
+                          0, 0, 1, 4);
         dialogGrid.attach(new Gtk.Label ({ label: "Title" }),
-                          0, 0, 1, 1);
-        dialogGrid.attach(this._newTitleEntry,
                           1, 0, 1, 1);
+        dialogGrid.attach(this._newTitleEntry,
+                          2, 0, 2, 1);
         dialogGrid.attach(new Gtk.Label ({ label: "Author" }),
-                          0, 1, 1, 1);
-        dialogGrid.attach(this._newAuthorEntry,
                           1, 1, 1, 1);
+        dialogGrid.attach(this._newAuthorEntry,
+                          2, 1, 2, 1);
+        dialogGrid.attach(new Gtk.Label ({ label: "ISBN" }),
+                          1, 2, 1, 1);
+        dialogGrid.attach(this._newISBNEntry,
+                          2, 2, 2, 1);
+        dialogGrid.attach(new Gtk.Label ({ label: "Year" }),
+                          1, 3, 1, 1);
+        dialogGrid.attach(this._newYearEntry,
+                          2, 3, 2, 1);
+        dialogGrid.attach(new Gtk.Label ({ label: "Rating" }),
+                          1, 4, 1, 1);
+        dialogGrid.attach(this._newRating,
+                          2, 4, 1, 1);
+        dialogGrid.attach(new Gtk.Label ({ label: "Summary" }),
+                          0, 5, 1, 1);
+        dialogGrid.attach(this._newSummaryTextArea,
+                          0, 6, 4, 1);
+        dialogGrid.attach(new Gtk.Label ({ label: "Notes" }),
+                          0, 7, 1, 1);
+        dialogGrid.attach(this._newNotesTextArea,
+                          0, 8, 4, 1);
 
 /*
- * ISBN
  * pixbuf (path)
- * year
- * summary
- * notes
- * rating
  * nie:tracker
  */
+
+
+        let cancelButton = new Gtk.Button.new_from_stock(Gtk.STOCK_CANCEL);
+        cancelButton.connect("clicked", Lang.bind(this, this.hide_on_delete));
+        dialogGrid.attach(cancelButton,
+                          2, 9, 1, 1);
 
         let okButton = new Gtk.Button.new_from_stock(Gtk.STOCK_OK);
         okButton.connect("clicked", Lang.bind (this, this._book_window_ok));
         dialogGrid.attach(okButton,
-                          0, 2, 2, 1);
+                          3, 9, 1, 1);
 
         this.add(dialogGrid);
         
